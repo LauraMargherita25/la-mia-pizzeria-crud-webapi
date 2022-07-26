a0aa313b82b3e90930a39c47ebd891f0ad432fdf
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-07-26 14:18:45 +0200

    Api controller with get controller action

---
+++ Controllers/Api/PizzaController.cs
@@ -0,0 +1,22 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers.Api
+{
+    [Route("api/Pizza")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        [Route("get")]
+        [HttpGet]
+        public IActionResult GetPizze()
+        {
+            PizzeriaContext ctx = new PizzeriaContext();
+            
+            IQueryable<Pizza> pizzaList = ctx.Pizze;
+            return Ok(pizzaList.ToList());
+            
+        }
+    }
+}
